4faa9626a7bed8a115051da01a9fd97859283e2a
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2024-11-10 20:28:20 -0500

    fixed naming

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,9 +1,9 @@
 package seg3x02.employeeGql.resolvers
 
-import org.springframework.stereotype.Controller
-import org.springframework.graphql.data.method.annotation.QueryMapping
-import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
 import seg3x02.employeeGql.entity.Employee
 import seg3x02.employeeGql.repository.EmployeesRepository
 import java.util.*
@@ -15,29 +15,33 @@ class EmployeesResolver(private val employeesRepository: EmployeesRepository) {
     fun employees(): List<Employee> = employeesRepository.findAll()
 
     @QueryMapping
-    fun employeeById(@Argument id: String): Employee? = employeesRepository.findById(id).orElse(null)
+    fun employeeById(@Argument id: String): Employee? =
+        employeesRepository.findById(id).orElse(null)
+
+    @QueryMapping
+    fun employeeByEmail(@Argument email: String): Employee? =
+        employeesRepository.findAll().find { it.email == email }
 
     @MutationMapping
     fun addEmployee(@Argument input: EmployeeInput): Employee {
         val newEmployee = Employee(
-            name = input.name,
-            dateOfBirth = input.dateOfBirth,
-            city = input.city,
-            salary = input.salary,
+            name = input.name ?: throw Exception("Please enter a name"),
+            dateOfBirth = input.dateOfBirth ?: throw Exception("Please enter a valid date of birth"),
+            city = input.city ?: throw Exception("Please enter a valid city"),
+            salary = input.salary ?: throw Exception("Please enter a valid salary"),
             gender = input.gender,
             email = input.email
         )
-        newEmployee.id = UUID.randomUUID().toString()  // Assign a unique ID
+        newEmployee.id = UUID.randomUUID().toString()
         return employeesRepository.save(newEmployee)
     }
-
 }
 
 data class EmployeeInput(
-    val name: String,
-    val dateOfBirth: String,
-    val city: String,
-    val salary: Float,
+    val name: String?,
+    val dateOfBirth: String?,
+    val city: String?,
+    val salary: Float?,
     val gender: String?,
     val email: String?
 )
